0f2884e58b095dc8d263dece2f14c4d3edd82bbd
张 悦 <yue.zhang@shuyun.com>
2016-06-09 15:49:44 +0800

    增加声明式事务

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -13,6 +13,9 @@ import org.seckill.exception.SeckillException;
 import org.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
@@ -25,12 +28,15 @@ import java.util.List;
  *
  * @author yue.zhang
  */
+@Service
 public class SeckillServiceImpl implements SeckillService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     private final String slat = "adfqwerjpasdfjlqwerasdf123498asdfl41";
@@ -69,6 +75,14 @@ public class SeckillServiceImpl implements SeckillService {
         return md5;
     }
 
+
+    /**
+     * 使用注解控制事务方法的优点：
+     * 1. 开发团队达成一致约定，明确标注事务方法的编程风格。
+     * 2. 保证事务方法的执行时间尽可能的短，不要穿插其他的网路操作RPC/HTTP请求或者剥离到事务方法外部。
+     * 3. 不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制。
+     */
+    @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
 
         if(md5 == null || !md5.equals(getMD5(seckillId))){
